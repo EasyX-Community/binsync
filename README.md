3bfb0f3a9ab70dd1ecdb1009fd2ec77be7c1b73b
mooleshacat <43627985+leshacat@users.noreply.github.com>
2022-01-30 20:32:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# binsync v1.0.4
+
+#### Description:
+Synchronize bin directory across multiple systems with rsync. Featuring a new installer. Linux only.
+
+Usage example: You have 5 raspberry pi's all running arm64 raspbian and want to save time compiling once for all machines<br />
+Usage example: You have 3 computer's all running amd64 debian and want to save time compiling once for all machines
+
+### Disclaimer:
+**I am not liable in any way for damages to your computer due to bugs, being hacked, exploited, or any other malfunction of the scripts. Having a directory with binaries automatically synchronized to every machine is inherently dangerous and can be exploited for malicious purposes. Please be advised of the risks and do not execute binaries that you did not place into the directory yourself. This source code is open, it is your responsibility to audit the code.**
+
+#### Notes:
+- It is important the instructions are completed in order!
+- Read the notes about [cronjobs](#cronjobs) before installing
+- You can only have one host machine under one root/user account
+- You can have as many client machines as you like, under any root/user account
+- Do not delete the bin directory unless you know what you're doing
+- Do not delete the git repository directory, it installs in it's place
+
+#### Gather client SSH keys:
+1. Login as non-root user
+2. `su` and enter root password if setting up root
+3. Generate the key `ssh-keygen` (leave all options including password empty and press enter)
+4. chmod 700 /root/.ssh/id_rsa
+4. Print the key `cat ${HOME}/.ssh/id_rsa.pub`
+5. Copy the line printed out into a notepad (yes it is one giant line)
+
+#### Install keys on host machine:
+1. Login as non-root user
+2. `su` and enter root password if setting up root
+3. `nano ${HOME}/.ssh/authorized_keys`
+4. Copy the lines from notepad and paste into the file
+5. Save & Exit
+
+#### On the host machine:
+1. Login as non-root user
+2. `su` and enter root password if setting up root as the host
+3. Change to directory you wish to install repository
+4. Clone repo `git clone https://gogs.easyx.cc/EasyX-Community/binsync.git`
+5. Change to repository directory `cd binsync`
+6. Execute installer `./install_binsync.sh`
+7. `source ~/.bashrc`
+8. Note: Do not run push/pull manually or set up cronjob on the host machine! The clients do the push/pull!
+
+#### On the client(s) machines:
+1. Login as non-root user
+2. `su` and enter root password if setting up root as the client
+3. Change to directory you wish to install repository
+4. Clone repo `git clone https://gogs.easyx.cc/EasyX-Community/binsync.git`
+5. Change to repository directory `cd binsync`
+6. Execute installer `./install_binsync.sh`
+7. `source ~/.bashrc`
+8. Run manually `binsync-push ; binsync-pull ;`
+
+#### Updating (manual):
+1. Run update script `binsync-update`
+
+#### Cronjobs:
+The installer will ask if you want it to install cronjob for you. It will also ask if you want it to install a weekly update cronjob for you.
+
+It is advised you select **'yes'** and then if you want to change it you can use `crontab -e` later.
+
+If you are unsure about crontab times, this calculator will come in handy [https://crontab.guru/](https://crontab.guru/)
+
+#### Coming features:
+- Can't think of any at the moment, but please create GitHub or Gogs issue ticket if you have any suggestions
+
+#### Donations:
+**XMR:** 84wwa7EKo8uasZAHijHKtBTuBaMPuNjCJgnfGJrsLFo4aZcfrzGvUX33sSeFNdno8fPiTDGnz4h1bCvsdFQYWRuR2619FzS <br />
+**ETH(ERC-20):** 0xc89eEa9b5C0cfa7f583dc1A6405a7d5730ADB603 <br />
+**BNB(BSC)** 0xc89eEa9b5C0cfa7f583dc1A6405a7d5730ADB603 <br />
+**RTM:** RDg5KstHYvxip77EiGhPKYNL3TZQr6456T <br />
+**AVN:** R9zSPpKjo6tCutMT5FyyGNr2vRaAssEtrm <br />
+**PHL:** F7XaUosKYEXPP62o31DdpDoADo3VcxoFP4 <br />
+**PEXA:** XBghzGLdeUzspUcJpeggPFLs3mAyTRHpPH <br />
---
+++ install_binsync.sh
@@ -0,0 +1,221 @@
+#!/usr/bin/env bash
+
+# Autofilled Variables (do not change)
+export vPWD=$(pwd)
+export vPATH=$(echo $PATH)
+export vVER=$(cat bin/.binsync.version)
+
+# Clear variables
+vUSER=""
+vHOST=""
+vBINSYNCDIR=""
+vHOSTMACHINE=1
+vCRONJOB=1
+vCRONJOBUPDATE=1
+vHOSTMACHINEENG=""
+vCRONJOBENG=""
+
+if [ -f "bin/.binsync.config" ] ; then
+
+  source bin/.binsync.config
+
+  echo ""
+  echo "binsync install ${vVER}"
+  echo ""
+  echo "user                [$vUSER]"
+  echo "host                [$vHOST]"
+#  echo "source directory    [$vSOURCEDIR]"
+  echo "working directory   [$vPWD]"
+  echo "bin directory       [$vBINSYNCDIR]"
+  echo "path                [$vPATH]"
+  echo ""
+
+else
+
+  echo "apt updating..."
+  apt update
+  echo ""
+
+  vRSYNC=$(dpkg-query -l rsync)
+  if [[ "no packages found matching" == *"${vRSYNC}"* ]]; then
+    echo "installing rsync"
+    echo ""
+    apt install -y rsync
+    echo ""
+  fi
+
+  vDIALOG=$(dpkg-query -l dialog)
+  if [[ "no packages found matching" == *"${vDIALOG}"* ]]; then
+    echo "installing dialog"
+    echo ""
+    apt install -y dialog
+    echo ""
+  fi
+
+  vCONTINUEVAR=1
+  while [ "$vCONTINUEVAR" == "1" ]
+  do
+
+    while [ "$vUSER" == "" ]
+    do
+      vUSER=$(dialog --stdout --title "Configuration" \
+        --backtitle "binsync ${vVER} setup" \
+        --inputbox "User:" 8 60)
+    done
+
+    while [ "$vHOST" == "" ]
+    do
+      vHOST=$(dialog --stdout --title "Configuration" \
+        --backtitle "binsync ${vVER} setup" \
+        --inputbox "Host IP:" 8 60)
+    done
+
+    while [ "$vBINSYNCDIR" == "" ]
+    do
+      vBINSYNCDIR=$(dialog --stdout --title "Configuration" \
+        --backtitle "binsync ${vVER} setup" \
+        --inputbox "Binsync Dir:" 8 60)
+    done
+
+    dialog --stdout --title "Configuration" \
+      --backtitle "binsync ${vVER} setup" \
+      --yesno "Is this the host machine?" 10 60 \
+    3>&1 1>&2 2>&3 3>&-
+    vHOSTMACHINE=$?
+
+    if [[ $vHOSTMACHINE -eq 1 ]] ; then
+
+      dialog --stdout --title "Configuration" \
+        --backtitle "binsync ${vVER} setup" \
+        --yesno "Should I install cronjob to run every 5 min?" 10 60 \
+      3>&1 1>&2 2>&3 3>&-
+      vCRONJOB=$?
+
+    fi
+
+    dialog --stdout --title "Configuration" \
+      --backtitle "binsync ${vVER} setup" \
+      --yesno "Should I install cronjob to update binsync weekly?" 10 60 \
+    3>&1 1>&2 2>&3 3>&-
+    vCRONJOBUPDATE=$?
+
+    if [ $vHOSTMACHINE -eq 0 ] ; then
+      vHOSTMACHINEENG="yes"
+    else
+      vHOSTMACHINEENG="no"
+    fi
+
+    if [ $vCRONJOB -eq 0 ] ; then
+      vCRONJOBENG="yes"
+    else
+      vCRONJOBENG="no"
+    fi
+
+    if [ $vCRONJOBUPDATE -eq 0 ] ; then
+      vCRONJOBUPDATEENG="yes"
+    else
+      vCRONJOBUPDATEENG="no"
+    fi
+
+    dialog --stdout --title "Configuration" \
+      --backtitle "binsync ${vVER} setup" \
+      --yesno "Is this information correct?\nUser: ${vUSER}\nHost IP: ${vHOST}\nBinsync Dir: ${vBINSYNCDIR}\nRepo Dir: ${vPWD}\nIs Host: ${vHOSTMACHINEENG}\nInstall Cronjob: ${vCRONJOBENG}\nWeekly update cronjob: ${vCRONJOBUPDATEENG}" 15 60 \
+    3>&1 1>&2 2>&3 3>&-
+    vCONTINUEVAR=$?
+
+  done
+
+  echo "" | tee bin/.binsync.config > /dev/null 2>&1
+  echo "#!/usr/bin/env bash" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "#" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "## SSH Connection Info" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "#" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "# Username for SSH connection" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "export vUSER=\"${vUSER}\"" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "# Host IP for SSH connection" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "export vHOST=\"${vHOST}\"" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "#" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "## Directories" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "#" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "# Location of binsync dir to sync (DO NOT put trailing /)" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "export vBINSYNCDIR=\"${vBINSYNCDIR}\" # DO NOT put trailing /" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "# Location of this repository" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "export vREPODIR=\"${vPWD}\"" | tee -a bin/.binsync.config > /dev/null 2>&1
+  echo "" | tee -a bin/.binsync.config > /dev/null 2>&1
+
+fi
+
+source bin/.binsync.config
+
+mkdir -p ${vBINSYNCDIR}
+
+# CHMOD the binsync scripts
+chmod +x bin/binsync-*
+
+# Check if vBINSYNCDIR is added to $PATH
+if [[ "${vPATH}" == *"${vBINSYNCDIR}"* ]]; then
+  echo "binsync repo directory already in path [${vPATH}]"
+  echo ""
+else
+  echo "Adding binsync repo directory to \$PATH"
+  echo "export PATH=${vBINSYNCDIR}:\$PATH" | tee -a ${HOME}/.bashrc
+  echo ""
+fi
+
+# Check if vPWD/bin is added to $PATH
+if [[ "${vPATH}" == *"${vPWD}/bin"* ]]; then
+  echo "binsync bin directory already in path [${vPATH}]"
+  echo ""
+else
+  echo "Adding binsync bin directory to \$PATH"
+  echo "export PATH=${vPWD}/bin:\$PATH" | tee -a ${HOME}/.bashrc
+  echo ""
+fi
+
+# ADD BINSYNC CRONJOB
+if [[ $vCRONJOB -eq 0 ]] ; then
+  vCRONJOBENG="yes"
+  vCRONENTRIES=$(crontab -l)
+  if [[ "$vCRONENTRIES" != *"${vPWD}/bin/binsync-pull ; ${vPWD}/bin/binsync-push ;"* ]] ; then
+    echo "installing cronjob"
+    echo -e "$(crontab -l)\n\n# binsync cronjob" | crontab -
+    echo -e "$(crontab -l)\n*/5 * * * * ${vPWD}/bin/binsync-pull ; ${vPWD}/bin/binsync-push ;" | crontab -
+  else
+    echo "refusing to install cron, cron already exists!"
+  fi
+else
+  vCRONJOBENG="no"
+  echo "skipping cronjob install"
+fi
+
+# ADD BINSYNC-UPDATE CRONJOB
+if [[ $vCRONJOBUPDATE -eq 0 ]] ; then
+  vCRONJOBUPDATEENG="yes"
+  vCRONENTRIES=$(crontab -l)
+  if [[ "$vCRONENTRIES" != *"@weekly ${vPWD}/bin/binsync-update ;"* ]] ; then
+    echo "installing binsync-update cronjob"
+    echo -e "$(crontab -l)\n\n# binsync-update cronjob" | crontab -
+    echo -e "$(crontab -l)\n@weekly ${vPWD}/bin/binsync-update ;" | crontab -
+  else
+    echo "refusing to install binsync-update cron, cron already exists!"
+  fi
+else
+  vCRONJOBUPDATEENG="no"
+  echo "skipping binsync-update cronjob install"
+fi
+
+echo ""
+
+echo "binsync installation is complete!"
+echo ""
+echo "Please execute 'source ~/.bashrc' or log out and back in again"
+echo ""
+
+# Clear ENV vars for security
+source bin/.binsync.config.clear
